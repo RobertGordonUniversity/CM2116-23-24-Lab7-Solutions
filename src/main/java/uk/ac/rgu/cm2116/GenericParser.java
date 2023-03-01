45884473cdb9b51764ac47c160ed66e70e23bf46
Mark Snaith <m.snaith@rgu.ac.uk>
2023-03-01 16:28:17 +0000

    Adding generic parser

--- src/main/java/uk/ac/rgu/cm2116/GenericParser.java
+++ src/main/java/uk/ac/rgu/cm2116/GenericParser.java
@@ -3,19 +3,29 @@ package uk.ac.rgu.cm2116;
 import uk.ac.rgu.cm2116.AbstractStack.StackOverflowException;
 import uk.ac.rgu.cm2116.AbstractStack.StackUnderFlowException;
 
-public class Parser {
+/**
+ * Generalised implementation of the parser. The pair of characters are given as constructor parameters
+ */
+public class GenericParser {
     
+    char c1, c2;
+
+    public GenericParser(char c1, char c2){
+        this.c1 = c1;
+        this.c2 = c2;
+    }
+
     public boolean parse(String input){
         Stack<Character> stack = new Stack<>();
         for(char c : input.toCharArray()){
-            if(c == '('){
+            if(c == this.c1){
                 try{
                     stack.push(c);
                 }catch(StackOverflowException ex){
                     System.out.println(ex.getMessage());
                     return false;
                 }
-            }else if(c == ')'){
+            }else if(c == this.c2){
                 try{
                     stack.pop();
                 }catch(StackUnderFlowException ex){
@@ -27,4 +37,5 @@ public class Parser {
         /* If we get this far, the stack has to be empty; if it's not, the parse fails */
         return stack.isEmpty();
     }
+
 }
